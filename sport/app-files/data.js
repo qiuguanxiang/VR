8d8efefe3397134ae9fa95e06c1c8a850fe264f9
qiuguanxiang <qiuguanxiang602@gmail.com>
2025-11-06 11:26:35 +0800

    add

---
+++ sport/app-files/data.js
@@ -0,0 +1,290 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "操場西側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8101469096186396,
+          "pitch": 0.06038571735786569,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.38956020379090894,
+          "pitch": 0.12629581314183547,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4540110127570793,
+          "pitch": -0.09607657380526291,
+          "title": "榕樹",
+          "text": "蜂窩<div><br></div>"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "司令台",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.44475954593685785,
+          "pitch": 0.13529283075991927,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.35706650402886453,
+          "pitch": 0.04578514234414044,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.621787796557955,
+          "pitch": 0.09350126206991582,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.4199729947162716,
+          "pitch": -0.06874717615549741,
+          "title": "司令台",
+          "text": "這裡只有開朝會時和校慶才會用到"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "排球場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.9196944017348905,
+          "pitch": 0.07795865045300943,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -2.7157001134896124,
+          "pitch": 0.05547030763788641,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.3910114123947146,
+          "pitch": 0.05178139479368227,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.3796885306982105,
+          "pitch": 0.5248031594096041,
+          "title": "鸚鵡",
+          "text": "鸚鵡，鸚形目（Psittaciformes）的鳥類，以其色彩鮮豔的羽毛、彎曲的喙以及模仿人類語言的能力而聞名。牠們是高度社會化、聰明的鳥類"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "操場東側",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.7812166966612502,
+          "pitch": 0.09541140324228436,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -3.118305902906423,
+          "pitch": 0.09343924031599826,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "籃球場",
+          "text": "籃球場，放學後開放到6:00早自習開放時間6:00-7:20"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "操場中段",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.375194115135887,
+          "pitch": 0.052368030541732224,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.0495932590839656,
+          "pitch": 0.10742869479639516,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.86447911773213,
+          "pitch": 0.05594476832488482,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 1.2670593572986402,
+          "pitch": 0.0975128467914228,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.39209642834181224,
+          "pitch": -0.28080215160032296,
+          "title": "斑鳩",
+          "text": "斑鳩是鳩鴿科斑鳩屬的中小型鳥類，學名為Streptopelia。牠們廣泛分布於亞洲、歐洲、非洲和澳洲等地的開闊林地、農田及城市區域。"
+        }
+      ]
+    }
+  ],
+  "name": "sport",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
---
+++ stree/app-files/data.js
@@ -0,0 +1,284 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "櫻花大道",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.3765566630079782,
+          "pitch": 0.24871049657245514,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.007221800128606404,
+          "pitch": 0.0015300262376491958,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.728923454576579,
+          "pitch": -0.2866995583721774,
+          "title": "櫻花",
+          "text": "櫻花是薔薇科李屬植物的泛稱，以其在春季盛開的粉紅色或白色花朵而聞名。雖然櫻花樹在北半球許多地區都有生長，但它特別與日本文化緊密相連，並被視為其國花。&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "1-",
+      "name": "榕樹",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.23380015106841867,
+          "pitch": 0.1081014246780807,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.4171037097586634,
+          "pitch": 0.04287681852130376,
+          "rotation": 0,
+          "target": "3-"
+        },
+        {
+          "yaw": -0.7777704123078237,
+          "pitch": 0.046033316796268764,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 2.842731441106409,
+          "pitch": 0.1425892762202139,
+          "rotation": 0,
+          "target": "0-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.347767923924712,
+          "pitch": -0.14672362689208107,
+          "title": "夜鷺",
+          "text": "夜鷺，鷺科鳥類，因常在夜間活動覓食，在臺灣俗稱「暗光鳥」。牠們是分布廣泛的留鳥，在水邊常可見到其身影。"
+        }
+      ]
+    },
+    {
+      "id": "2-",
+      "name": "停車場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.1707001021415326,
+          "pitch": 0.2551113998398762,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.9492385095156246,
+          "pitch": 0.03800526308217833,
+          "rotation": 1.5707963267948966,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9366001085954982,
+          "pitch": -0.029746281246659123,
+          "title": "生態池",
+          "text": "生態池，裡面的魚皆為校內老師放養，更夜鷺覓食"
+        }
+      ]
+    },
+    {
+      "id": "3-",
+      "name": "足球場間排球場",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -3.00176090160544,
+          "pitch": 0.11030176215507481,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.3518390302220045,
+          "pitch": 0.040271600636092586,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.8114387689573483,
+          "pitch": 0.24170553361035552,
+          "title": "球場",
+          "text": "這裡平時供學生踢足球和打排球，但校慶時這裡就會變成學生的擺攤地點&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "4-",
+      "name": "活動中心門口",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.8332883900939265,
+          "pitch": 0.15094931409951506,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -3.0940986450839496,
+          "pitch": 0.15117809945326144,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.8388401941741677,
+          "pitch": 0.24207398306023364,
+          "rotation": 0,
+          "target": "3-"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.659358337370552,
+          "pitch": -0.11135954138579685,
+          "title": "活動中心",
+          "text": "活動中心內有器材室，桌球場，羽球場，籃球場同時這裡也是朝會的雨天備案的場地"
+        }
+      ]
+    }
+  ],
+  "name": "stree",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
