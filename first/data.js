b7a19be5fef7243fb0b39378f0ebdf1da2d74001
qiuguanxiang <qiuguanxiang602@gmail.com>
2025-11-06 11:46:21 +0800

    .

---
+++ first/data.js
@@ -0,0 +1,49 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "校門",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1414,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0.960974071894789,
+          "pitch": 0.14517520089272296,
+          "title": "鯉魚",
+          "text": "鯉魚是一種常見的淡水魚，原產於歐亞大陸，在世界各地廣泛分布，包括台灣的河川中下游、湖泊和池塘。 牠們是雜食性魚類，適應性強，多棲息於水域中下層的靜水區，是常見的食用魚。&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "first",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
